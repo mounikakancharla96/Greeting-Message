7efa4fed0a083e818dce29e3654db208497f7982
Kancharla Mounika <kancharlamounika17@gmail.com>
2022-11-04 04:07:46 +0000

    greeting message

---
+++ message/importMessage.js
@@ -0,0 +1,3 @@
+const messg = require("./index")
+
+console.log(messg)
---
+++ message/index.js
@@ -0,0 +1,5 @@
+const msg = require("../greeting/index")
+
+const new_msg = "Hello Rahul!"
+
+module.exports = `${new_msg} ${msg}`
